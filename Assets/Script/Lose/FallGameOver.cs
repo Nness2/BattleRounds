8e9ceecdfe523ad00711f2811aeb48805994011d
Aness Menaa <menaa.aness2@gmail.com>
2021-05-10 18:51:28 +0200

    All Level Done

--- Assets/Script/Lose/FallGameOver.cs
+++ Assets/Script/Lose/FallGameOver.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class FallGameOver : MonoBehaviour
 {
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "player")
+        {
+            Destroy(collision.collider.gameObject);
+            Debug.Log("You Lose");
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Script/Lose/LavaGameOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaGameOver : MonoBehaviour
+{
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.tag == "player")
+        {
+           Destroy(collision.collider.gameObject);
+           Debug.Log("You Lose");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
