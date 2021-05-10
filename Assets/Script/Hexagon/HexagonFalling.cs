468b5e5f554b6322863184eb7b83392519909043
Aness Menaa <menaa.aness2@gmail.com>
2021-05-10 18:29:41 +0200

    Hexagon level Done

---
+++ Assets/Script/Hexagon/HexagonFalling.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonFalling : MonoBehaviour
+{
+    private IEnumerator selfDelete;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "player")
+        {
+            transform.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+            StartCoroutine(selfDelete);
+        }
+    }
+
+    private void Start()
+    {
+        selfDelete = TimeDelete(0.5f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator TimeDelete(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Destroy(transform.gameObject);
+    }
+}
